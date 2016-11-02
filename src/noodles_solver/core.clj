1cf7c49cdded960c1b706e97d762c15d6dad60f7
Kenneth L. Smith <ken.smith@2scientists.net>
2016-11-01 18:51:32 -0600

    Add initial direction manipulation functions

--- src/noodles_solver/core.clj
+++ src/noodles_solver/core.clj
@@ -1,5 +1,47 @@
 (ns noodles-solver.core
+  (:require [clojure.set :as set])
   (:gen-class))
+
+(def directions-sq [:n :e :s :w])
+(def directions-hex [:n :ne :se :s :sw :nw])
+
+(defn mirror-directions
+  "Creates a mirrored (180 degree rotation) collection of
+   directions based on a vector of directions"
+  [dirs]
+  (let [n (/ (count dirs) 2)]
+    (lazy-cat (drop n dirs) (take n dirs))))
+
+(def mirrors-sq
+  "A map from the square-grid directions to their mirror directions"
+  (zipmap directions-sq (mirror-directions directions-sq)))
+
+(defn possible-orientations
+  [piece]
+  (let [{type :type} piece]
+    (case type
+      :cap [#{:n} #{:e} #{:w} #{:s}]
+      :corner [#{:n :e} #{:e :s} #{:s :w} #{:w :n}]
+      :line [#{:n :s} #{:e :w}]
+      :tee [#{:n :e :s} #{:e :s :w} #{:s :w :n} #{:w :n :e}])))
+
+(defn has-no-forbidden? [excls orientation]
+  "Returns true if a piece's orientation points in any excluded directions"
+  (empty? (set/intersection excls orientation)))
+
+(defn has-required? [incls orientation]
+  "Returns true if a piece's orientation points in all required directions"
+  (set/subset? incls orientation))
+
+(defn satisfies-constraints? [constraints orientation]
+  "Returns true if a piece's orientation points in all required directions
+  and does not point in any excluded directions"
+  (let [{excls :forbidden incls :required} constraints]
+    (and
+      (has-required? incls orientation)
+      (has-no-forbidden? excls orientation))))
+
+
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/noodles_solver/core_test.clj
+++ test/noodles_solver/core_test.clj
@@ -2,6 +2,14 @@
   (:require [clojure.test :refer :all]
             [noodles-solver.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest mirror-test
+  (testing "Should return the mirrored compass headings"
+    (is (= (mirror-directions directions-sq) [:s :w :n :e]))
+    (is (= (mirror-directions directions-hex) [:s :sw :nw :n :ne :se]))))
+
+
+(deftest has-required-test
+  (testing "has-required returns expected values"
+    (is (has-required? #{:n :e} #{:n :e :s}))
+    (is (has-required? #{:n :e :s} #{:n :e :s}))
+    (is (not (has-required? #{:w} #{:n :e :s})))))
